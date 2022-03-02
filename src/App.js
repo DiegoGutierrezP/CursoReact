a59cf99cf0175a1b188badaec31b1420a5ab0399
Diego <diego.sk75.dgp@gmail.com>
2022-03-01 19:58:45 -0500

    clase 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+//JSX
 function App() {
+  let nombre = "diego";
+  let auth = false;
+  let estaciones = ["primavera","verano","otonio","invierno"];
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <label htmlFor='nombre'>Nombre</label>
+          <input type="text" id='nombre'></input>
+          <h1>{nombre}</h1>
+          <p>{auth? "El usuario esta logeado": "El usuario no esta logeado"}</p>
+        <p>{2+1}</p>  
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editar <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          {
+            estaciones.map((el,index) => <li key={index}>{el}</li>)
+          }
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +33,8 @@ function App() {
         </a>
       </header>
     </div>
+    <div></div>
+    </>
   );
 }
 
